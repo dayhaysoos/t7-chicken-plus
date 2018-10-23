1f6ca708e89f48b58e2b0fc0f5b46c1e5f2858d3
Nick DeJesus <nickdejesus@Nicks-MacBook-Pro.local>
2018-10-23 00:33:09 -0400

    new components

---
+++ src/components/BottomMenuBar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import FontAwesome, { Icons } from 'react-native-fontawesome';
+
+const MainContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: transparent;
+`;
+
+const MenuButton = styled.TouchableOpacity`
+  height: 60;
+  width: 75;
+`;
+
+const MenuIconText = styled.Text`
+  color: red;
+  text-align: center;
+`;
+
+const MenuIcon = styled(FontAwesome)`
+  font-size: 30;
+`;
+
+const MenuLabelText = styled.Text`
+  color: white;
+  font-size: 12;
+  text-align: center;
+`;
+
+const BottomMenuBar = ({ isListView, onPressFilterMenu, navigation, onPressFavoriteFilter, toggleListView }) => (
+    <MainContainer>
+        {navigation && (
+            <MenuButton onPress={() => navigation.toggleDrawer()} >
+                <MenuIconText>
+                    <MenuIcon>{Icons.bars}</MenuIcon>
+                </MenuIconText>
+                <MenuLabelText>Menu</MenuLabelText>
+            </MenuButton>
+        )}
+
+        {onPressFavoriteFilter && (
+            <MenuButton onPress={onPressFilterMenu}>
+                <MenuIconText>
+                    <MenuIcon>Derp</MenuIcon>
+                </MenuIconText>
+                <MenuLabelText>Toggle Favorites</MenuLabelText>
+            </MenuButton>
+        )}
+
+        {toggleListView && (
+            <MenuButton onPress={toggleListView}>
+                <MenuIconText>
+                    <MenuIcon>{isListView ? Icons.th : Icons.list}</MenuIcon>
+                </MenuIconText>
+                <MenuLabelText>Switch View</MenuLabelText>
+            </MenuButton>
+        )}
+
+        {onPressFilterMenu && (
+            <MenuButton onPress={onPressFilterMenu}>
+                <MenuIconText>
+                    <MenuIcon>{Icons.filter}</MenuIcon>
+                </MenuIconText>
+                <MenuLabelText>Filter Menu</MenuLabelText>
+            </MenuButton>
+        )}
+
+    </MainContainer>
+);
+
+BottomMenuBar.propTypes = {
+    onPressFilterMenu: PropTypes.func,
+    onPressFavoriteFilter: PropTypes.func,
+    toggleListView: PropTypes.func,
+    navigation: PropTypes.object,
+    isListView: PropTypes.bool
+};
+
+export default BottomMenuBar;
---
+++ src/components/FilterOption.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class FilterOption extends Component {
+  state = {
+      active: false
+  }
+
+  styles = {
+      check: {
+          color: '#2bbd27',
+          fontSize: 20
+      },
+      filterTouchable: {
+          borderColor: '#4E4E52',
+          borderBottomWidth: 1,
+      },
+      innerContainer: {
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          padding: 10,
+      },
+      text: {
+          color: 'white',
+          fontSize: 20
+      }
+  }
+
+  componentDidUpdate(prevProps) {
+      if (this.props.noActiveFilters && !prevProps.noActiveFilters) {
+          this.setState({ active: false });
+      }
+  }
+
+  render() {
+      return (
+          <TouchableHighlight
+              onPress={() => {
+                  // If clicking the filter will make it active
+                  if (!this.state.active) {
+                      this.props.addToActiveFilters(this.props.filter);
+                  } else {
+                      this.props.removeFromActiveFilters(this.props.filter);
+                  }
+
+                  this.setState({ active: !this.state.active });
+              }}
+              style={this.styles.filterTouchable}
+          >
+              <View style={this.styles.innerContainer}>
+                  <Text style={this.styles.text}>{this.props.text}</Text>
+                  {this.state.active && <Text style={this.styles.check}>✓</Text>}
+              </View>
+          </TouchableHighlight>
+      );
+  }
+}
+
+FilterOption.propTypes = {
+    addToActiveFilters: PropTypes.func.isRequired,
+    filter: PropTypes.func.isRequired,
+    noActiveFilters: PropTypes.bool.isRequired,
+    removeFromActiveFilters: PropTypes.func.isRequired,
+    text: PropTypes.string.isRequired
+};
