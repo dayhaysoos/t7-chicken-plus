591bdd806a7604ba175df1e2d3308d53e89d42b3
Shaheer Mir <shaheermir0@gmail.com>
2018-11-03 16:00:50 -0400

    Remove charData from persistence.

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,6 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key: 'root',
     storage,
-    //whitelist: ['characterData', 'settings'],
     whitelist: ['settings'],
 };
 
