b2d4ab5aa6553d86e9f0446f36f6c7d42c3f32ae
Shaheer Mir <shaheermir0@gmail.com>
2018-11-12 00:31:15 -0500

    Refactors CharacterBanner for animation support. Clean up CharProfile.js

--- src/containers/CharacterProfile.js
+++ src/containers/CharacterProfile.js
@@ -14,8 +14,6 @@ import * as characterActions from '../redux/actions/characterActions';
 import * as settingsActions from '../redux/actions/settingsActions';
 import { getCharacterMoveList } from '../selectors/characterSelect';
 
-import { characterBanners } from '../constants/characterBanners';
-
 import { GradientTheme } from '../common/GradientTheme';
 
 import Drawer from 'react-native-drawer';
@@ -25,12 +23,6 @@ import DrawerSwitcher from '../components/DrawerSwitcher';
 import BottomMenuBar from '../components/BottomMenuBar';
 import FilterMenu from '../components/FilterMenu';
 import { white } from 'ansi-colors';
-
-/*************** */
-const HEADER_MAX_HEIGHT = 300;
-const HEADER_MIN_HEIGHT = 0;
-const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
-/*************** */
 
 export const mapDispatcthToProps = {
     ...characterActions,
@@ -43,8 +35,11 @@ export const mapStateToProps = ({ characterData, theme, settings: { listView } }
     theme
 });
 
-const MainContainer = styled(Drawer)`
+const HEADER_MAX_HEIGHT = 300;
+const HEADER_MIN_HEIGHT = 0;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
+const MainContainer = styled(Drawer)`
 `;
 
 const EmptyText = styled.Text`
@@ -70,10 +65,7 @@ class CharacterProfile extends Component {
         isRightDrawerOpen: false,
         side: 'right',
         unFilteredMoveList: [],
-        ///
-
         scrollY: new Animated.Value(0),
-
     }
 
     componentDidMount() {
@@ -144,21 +136,15 @@ class CharacterProfile extends Component {
 
     render() {
         const { navigation, navigation: { state: { params: { name } } }, toggleListView, listView, theme } = this.props;
-        const { isOpen, side } = this.state;
+        const { isOpen, side, scrollY } = this.state;
 
-        const scrollY = Animated.add(
-            this.state.scrollY,
-            //Platform.OS === 'ios' ? HEADER_MAX_HEIGHT : 0,
-            Platform.OS === 'ios' ? 0 : 0,
-        );
         const headerTranslate = scrollY.interpolate({
             inputRange: [0, HEADER_SCROLL_DISTANCE],
             outputRange: [0, -HEADER_SCROLL_DISTANCE],
             extrapolate: 'clamp',
         });
-
         const imageOpacity = scrollY.interpolate({
-            inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+            inputRange: [0, HEADER_SCROLL_DISTANCE / 4, HEADER_SCROLL_DISTANCE],
             outputRange: [1, 1, 0],
             extrapolate: 'clamp',
         });
@@ -185,49 +171,27 @@ class CharacterProfile extends Component {
                 >
                     <GradientTheme theme={theme}>
                         <MainContainer>
-                            <Animated.View
-                                pointerEvents="none"
-                                style={[
-                                    styles.header,
-                                    {zIndex: 999},
-                                    { transform: [{ translateY: headerTranslate }] },
-                                ]}
-                            >
-                                <Animated.Image
-                                    style={[
-                                        styles.backgroundImage,
-                                        {
-                                            opacity: imageOpacity,
-                                            transform: [{ translateY: imageTranslate }],
-                                        },
-                                    ]}
-                                    source={characterBanners.akuma}
-                                />
-                            </Animated.View>
-                            <ScrollView
-                                style={{borderColor: white, borderWidth: 1, flex: 1}}
-                                stickyHeaderIndices={[0]}
+                            <CharacterBanner
+                                name={name.toLowerCase()}
+                                imageOpacity={imageOpacity}
+                                headerTranslate={headerTranslate}
+                                imageTranslate={imageTranslate}
+                            />
+                            <Animated.ScrollView
+                                stickyHeaderIndices={listView ? [] : [0]}
                                 contentContainerStyle={{paddingTop: HEADER_MAX_HEIGHT}}
                                 onScroll={Animated.event(
-                                    [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+                                    [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}],
+                                    {useNativeDriver: true}
                                 )}
+                                scrollEventThrottle={16}
                             >
-                                <Animated.View >
-                                    <HeaderRow />
-                                </Animated.View>
-                                <Animated.FlatList
+
+                                { !listView && <HeaderRow /> }
+
+                                <FlatList
                                     style={{flex: 1}}
-                                    // contentInset={{
-                                    //     top: HEADER_MAX_HEIGHT,
-                                    // }}
-                                    // contentOffset={{
-                                    //     y: -HEADER_MAX_HEIGHT,
-                                    // }}
-                                    // onScroll={Animated.event(
-                                    //     [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
-                                    //     { useNativeDriver: true },
-                                    // )}
-                                    contentContainerStyle={{ justifyContent: 'center', flexDirection: 'column' }}
+                                    contentContainerStyle={{ justifyContent: 'center', flexDirection: 'column', zIndex: 999 }}
                                     data={this.state.moveListArray}
                                     numColumns={1}
                                     keyExtractor={(item, index) => index.toString()}
@@ -237,16 +201,13 @@ class CharacterProfile extends Component {
                                         <SpreadSheetRow item={item} theme={theme} navigation={navigation} />
                                     )}
                                     ListEmptyComponent={() => <EmptyText>No results for this combination of Search and Filters</EmptyText>}
-                                    //ListHeaderComponent={listView ? <CharacterBanner name={name.toLowerCase()} /> : <HeaderRow />}
-                                    ListHeaderComponent={listView ? null : null}
-                                    initialNumToRender={3}
+                                    initialNumToRender={5}
                                     initialScrollIndex={0}
                                     getItemLayout={(item, index) => (
                                         { length: listView ? 120 : 100, offset: listView ? 120 : 100 * index, index }
                                     )}
-                                //stickyHeaderIndices={listView ? [] : [0]}
                                 />
-                            </ScrollView>
+                            </Animated.ScrollView>
                             <BottomMenuBar
                                 isListView={listView}
                                 navigation={navigation}
@@ -263,27 +224,3 @@ class CharacterProfile extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatcthToProps)(CharacterProfile);
-
-const styles = StyleSheet.create({
-    header: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        overflow: 'hidden',
-        height: HEADER_MAX_HEIGHT,
-    },
-    title: {
-        color: 'white',
-        fontSize: 18,
-    },
-    backgroundImage: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        width: null,
-        height: HEADER_MAX_HEIGHT,
-        resizeMode: 'cover',
-    },
-});
