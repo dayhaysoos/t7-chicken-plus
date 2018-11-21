c550628bb35ddfb84702c9d6f6b16881d7c9bb28
Nick DeJesus <ndejesus1227@gmail.com>
2018-11-21 00:46:40 -0500

    left behind things

--- android/app/src/main/java/com/t7chickenplus/MainApplication.java
+++ android/app/src/main/java/com/t7chickenplus/MainApplication.java
@@ -3,6 +3,7 @@ package com.t7chickenplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sbugert.rnadmob.RNAdMobPackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAdMobPackage(),
             new RNIapPackage(),
             new RNFirebasePackage(),
             new LinearGradientPackage(),
---
+++ src/components/AdBanner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AdMobBanner } from 'react-native-admob';
+
+const AdBanner = () => (
+    <AdMobBanner
+        adSize='fullBanner'
+        adUnitID={'ca-app-pub-2131722019779391~1691902228'}
+    />
+);
+
+export default AdBanner;
