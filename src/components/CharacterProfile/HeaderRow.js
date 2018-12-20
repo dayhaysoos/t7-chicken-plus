94153a5f76bbe3d37321075284e68a32698d50e1
Nick DeJesus <ndejesus1227@gmail.com>
2018-12-19 21:22:36 -0500

    styled-components upgraded, bug fixed

--- src/components/CharacterProfile/HeaderRow.js
+++ src/components/CharacterProfile/HeaderRow.js
@@ -10,26 +10,24 @@ const SpreadsheetCell = styled.Text`
   width: 75;
   border-width: 1;
   color: ${({ theme: { text } }) => text};
-  background-color: ${({ theme: { primaryGradient2 } }) => primaryGradient2}
+  background-color: ${({ theme: { primaryGradient2 } }) => primaryGradient2};
 `;
 
-const NotationCell = styled(SpreadsheetCell)`
+
+const HeaderNotationCell = styled(SpreadsheetCell)`
   width: 200;
 `;
 
-const HeaderNotationCell = styled(NotationCell)`
-`;
-
 const HeaderRow = () => (
-  <NotationRow>
-    <HeaderNotationCell>Notation</HeaderNotationCell>
-    <SpreadsheetCell>Hit Level(s)</SpreadsheetCell>
-    <SpreadsheetCell>Damage</SpreadsheetCell>
-    <SpreadsheetCell>Speed</SpreadsheetCell>
-    <SpreadsheetCell>On Block</SpreadsheetCell>
-    <SpreadsheetCell>On Hit</SpreadsheetCell>
-    <SpreadsheetCell>Counter Hit</SpreadsheetCell>
-  </NotationRow>
+    <NotationRow>
+        <HeaderNotationCell>Notation</HeaderNotationCell>
+        <SpreadsheetCell>Hit Level(s)</SpreadsheetCell>
+        <SpreadsheetCell>Damage</SpreadsheetCell>
+        <SpreadsheetCell>Speed</SpreadsheetCell>
+        <SpreadsheetCell>On Block</SpreadsheetCell>
+        <SpreadsheetCell>On Hit</SpreadsheetCell>
+        <SpreadsheetCell>Counter Hit</SpreadsheetCell>
+    </NotationRow>
 );
 
 export default HeaderRow;
