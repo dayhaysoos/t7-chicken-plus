16cd335ba050f65c7b37ea08f32a300fc415c215
Nick DeJesus <ndejesus1227@gmail.com>
2018-12-22 16:51:53 -0500

    remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from './reducers/index';
 
 import storage from 'redux-persist/lib/storage';
@@ -25,7 +25,7 @@ let store = createStore(
     composeEnhancers(
         applyMiddleware(
             thunk,
-            logger
+            // logger
         ),
     ),
 );
