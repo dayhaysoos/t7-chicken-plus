31c3842114eab16adf4035b8b8c9d29fb6c7ab6c
Nick DeJesus <ndejesus1227@gmail.com>
2018-12-26 01:22:08 -0500

    removing admob again -_-

--- android/app/src/main/java/com/t7chickenplus/MainApplication.java
+++ android/app/src/main/java/com/t7chickenplus/MainApplication.java
@@ -3,7 +3,6 @@ package com.t7chickenplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.sbugert.rnadmob.RNAdMobPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.dooboolab.RNIap.RNIapPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -38,7 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAdMobPackage(),
             new RNGestureHandlerPackage(),
             new RNIapPackage(),
             new SplashScreenReactPackage(),
