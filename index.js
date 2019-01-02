a625be53756f149bad66b1dc105b025982177e7e
Nick DeJesus <ndejesus1227@gmail.com>
2019-01-02 15:20:58 -0500

    migrate claudio

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { AppRegistry } from 'react-native';
 
 import App from './src/App';
 import { YellowBox } from 'react-native';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Require cycle:']);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Require cycle']);
 AppRegistry.registerComponent('t7chickenplus', () => App);
