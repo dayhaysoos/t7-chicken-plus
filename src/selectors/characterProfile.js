db1b30e7338de5386392da611b77975c977241b6
Nick DeJesus <ndejesus1227@gmail.com>
2019-02-12 10:12:26 -0500

    little change that makes more sense

--- src/selectors/characterProfile.js
+++ src/selectors/characterProfile.js
@@ -1,4 +1,3 @@
-import createSelector from 'selectorator';
 import * as filters from '../utils/filterFuncs';
 
 /**
@@ -13,7 +12,7 @@ export const filterMoves = (characterMoves, activeFilters) => {
     if (activeFilters.length === 0) {
         return characterMoves;
     } else {
-        return activeFilters.reduce((activeFilters, currentFilter) => {
+        return activeFilters.reduce((acc, currentFilter) => {
             const { filterProperty, filterType } = currentFilter;
             return filters[filterProperty].filters[filterType].filterFunction(characterMoves, filterProperty);
         }, []);
