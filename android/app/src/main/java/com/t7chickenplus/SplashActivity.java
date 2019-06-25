31ce75579fb5651b6207daaa3f52990a06c20b91
Nick DeJesus <ndejesus1227@gmail.com>
2019-06-25 15:48:27 -0400

    AndroidX changes

--- android/app/src/main/java/com/t7chickenplus/SplashActivity.java
+++ android/app/src/main/java/com/t7chickenplus/SplashActivity.java
@@ -3,7 +3,7 @@ package com.t7chickenplus; // make sure this is your package name
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
