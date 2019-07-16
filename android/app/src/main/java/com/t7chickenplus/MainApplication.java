0e1c58954c773f03181fe1aa222317bc2163b32c
Nick DeJesus <ndejesus1227@gmail.com>
2019-07-16 14:44:51 -0400

    missed things somehow

--- android/app/src/main/java/com/t7chickenplus/MainApplication.java
+++ android/app/src/main/java/com/t7chickenplus/MainApplication.java
@@ -3,18 +3,10 @@ package com.t7chickenplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import io.invertase.firebase.RNFirebasePackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.gettipsi.stripe.StripeReactPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.dooboolab.RNIap.RNIapPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.admob.RNFirebaseAdMobPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -41,17 +33,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReanimatedPackage(),
             new AsyncStoragePackage(),
             new RNFirebasePackage(),
-            new RNCWebViewPackage(),
-            new StripeReactPackage(),
-            new RNGestureHandlerPackage(),
-            new RNIapPackage(),
-            new SplashScreenReactPackage(),
             new RNFirebaseAnalyticsPackage(),
             new RNFirebaseAdMobPackage(),
-            new LinearGradientPackage(),
             new CodePush("XybHZshAtSxHCqD3lQKJ_WVKyWtu49811cfd-9167-45ec-81da-4173a98555d0", getApplicationContext(), BuildConfig.DEBUG)
       );
     }
