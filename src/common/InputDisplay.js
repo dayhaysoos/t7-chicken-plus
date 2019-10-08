d0295016ae57b340fca636eb3ae537303f9b0e86
dayhaysoos <ndejesus1227@gmail.com>
2019-10-08 19:54:28 -0400

    new input images

--- src/common/InputDisplay.js
+++ src/common/InputDisplay.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 import { INPUT } from '../constants/inputMap';
+import styled from 'styled-components'
+
+const InputContainer = styled.View`
+  flex: 1;
+  flexDirection: row;
+  justify-content: flex-start;
+  margin-top: 20;
+  margin-bottom: 10;
+`;
+
+const InputImage = styled.Image`
+  height: 20;
+  width: 20;
+  margin-right: 5;
+`;
 
 const InputMappings = ({ notation }) => {
-    inputs = notation.split(/(?<=[a-zA-Z])\+|[\s,]+/)
+    inputs = notation.replace(/[{()}]/g, '').split(/(?<=[a-zA-Z])\+|[\s,]+/)
 
     return (
         inputs.map((input, index) => {
-            return <Image key={index} source={INPUT[input]} />
+            return (
+                !INPUT[input] ? null : <InputImage key={index} source={INPUT[input]} />
+            )
         })
     )
 }
 
 const InputDisplay = ({ notation }) => {
     return (
-        <View>
+        <InputContainer>
             <InputMappings notation={notation} />
-        </View>
+        </InputContainer>
     )
 }
 
--- src/constants/inputMap.js
+++ src/constants/inputMap.js
@@ -1,12 +1,12 @@
 import createConstants from 'namespace-constants'
-import db from '../../assets/images/inputs/db.png'
 import one from '../../assets/images/inputs/1.png'
 import onePlusTwo from '../../assets/images/inputs/1plus2.png'
-import onePlusTwoPlusThree from '../../assets/images/inputs//1plus2plus3.png'
-import onePlusTwoPlusThreePlusFour from '../../assets/images/inputs//1plus2plus3plus4.png'
-import onePlusTwoPlusFour from '../../assets/images/inputs//1plus2plus4.png'
-import onePlusThree from '../../assets/images/inputs//1plus3.png'
-import onePlusThreePlusFour from '../../assets/images/inputs//1plus3plus4.png'
+import onePlusTwoPlusThree from '../../assets/images/inputs/1plus2plus3.png'
+import onePlusTwoPlusThreePlusFour from '../../assets/images/inputs/1plus2plus3plus4.png'
+import onePlusTwoPlusFour from '../../assets/images/inputs/1plus2plus4.png'
+import onePlusFour from '../../assets/images/inputs/1plus4.png'
+import onePlusThree from '../../assets/images/inputs/1plus3.png'
+import onePlusThreePlusFour from '../../assets/images/inputs/1plus3plus4.png'
 
 // two 
 import two from '../../assets/images/inputs/2.png'
@@ -24,6 +24,24 @@ import four from '../../assets/images/inputs/4.png'
 // directionals
 import b_h from '../../assets/images/inputs/b_h.png'
 import b from '../../assets/images/inputs/b.png'
+import d_h from '../../assets/images/inputs/d_h.png'
+import d from '../../assets/images/inputs/d.png'
+import db_h from '../../assets/images/inputs/db_h.png'
+import db from '../../assets/images/inputs/db.png'
+import df_h from '../../assets/images/inputs/df_h.png'
+import df from '../../assets/images/inputs/df.png'
+import f_h from '../../assets/images/inputs/f_h.png'
+import f from '../../assets/images/inputs/f.png'
+import u_h from '../../assets/images/inputs/u_h.png'
+import u from '../../assets/images/inputs/u.png'
+import uf_h from '../../assets/images/inputs/uf_h.png'
+import uf from '../../assets/images/inputs/uf.png'
+import ub_h from '../../assets/images/inputs/ub_h.png'
+import ub from '../../assets/images/inputs/ub.png'
+
+// just frame
+import star from '../../assets/images/inputs/star.png'
+
 
 
 export const INPUT_IMAGES = createConstants([
@@ -32,8 +50,8 @@ export const INPUT_IMAGES = createConstants([
     '1+2+3',
     '1+2+4',
     '1+3',
+    '1+4',
     '1+3+4',
-    'd/b',
     '2',
     '2+3',
     '2+3+4',
@@ -41,8 +59,23 @@ export const INPUT_IMAGES = createConstants([
     '3',
     '3+4',
     '4',
-    'b_h',
-    'b'
+    'B',
+    'b',
+    'F',
+    'f',
+    'd',
+    'D',
+    'd/b',
+    'D/B',
+    'd/f',
+    'D/F',
+    'd',
+    'D',
+    'u/b',
+    'U/B',
+    'u/f',
+    'U/F',
+    ':'
 ]);
 
 export const INPUT = {
@@ -52,6 +85,7 @@ export const INPUT = {
     [INPUT_IMAGES['1+2+3+4']]: onePlusTwoPlusThreePlusFour,
     [INPUT_IMAGES['1+2+4']]: onePlusTwoPlusFour,
     [INPUT_IMAGES['1+3']]: onePlusThree,
+    [INPUT_IMAGES['1+4']]: onePlusFour,
     [INPUT_IMAGES['1+3+4']]: onePlusThreePlusFour,
     [INPUT_IMAGES['2']]: two,
     [INPUT_IMAGES['2+3']]: twoPlusThree,
@@ -60,9 +94,23 @@ export const INPUT = {
     [INPUT_IMAGES['3']]: three,
     [INPUT_IMAGES['3+4']]: threePlusFour,
     [INPUT_IMAGES['4']]: four,
-    [INPUT_IMAGES['d/b']]: db,
     [INPUT_IMAGES['b']]: b,
-    [INPUT_IMAGES['b*']]: b_h,
+    [INPUT_IMAGES['B']]: b_h,
+    [INPUT_IMAGES['d']]: d,
+    [INPUT_IMAGES['D']]: d_h,
+    [INPUT_IMAGES['d/b']]: db,
+    [INPUT_IMAGES['D/B']]: db_h,
+    [INPUT_IMAGES['d/f']]: df,
+    [INPUT_IMAGES['D/F']]: df_h,
+    [INPUT_IMAGES['f']]: f,
+    [INPUT_IMAGES['F']]: f_h,
+    [INPUT_IMAGES['u']]: u,
+    [INPUT_IMAGES['U']]: u_h,
+    [INPUT_IMAGES['u/f']]: uf,
+    [INPUT_IMAGES['U/F']]: uf_h,
+    [INPUT_IMAGES['u/b']]: ub,
+    [INPUT_IMAGES['U/B']]: ub_h,
+    [INPUT_IMAGES[':']]: star,
 }
 
 
